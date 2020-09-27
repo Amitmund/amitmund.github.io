effcc6c4dbf6ea0b61e7a79bedccec7aea96b3de
amund <amund@akamai.com>
2020-09-27 21:04:44 +0530

    update

--- scripts/golangLinks.js
+++ scripts/golangLinks.js
@@ -1,57 +1,31 @@
-// https://stackoverflow.com/questions/17600264/why-jquery-click-doesnt-work-when-included-in-a-separate-file
+// You can put individual links in the following main block.
+// Make sure to use '.' for class and '#' for id
 
 $(document).ready(function(){
 
-	$('.golangIntroduction').click(function(){
+	// for golangIntroduction class.
+	$('#golangIntroduction').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html')
 	});
 
-	$('.golangEnvironmentSetup').click(function(){
+	// and so on for rest of the links.
+	$('#golangEnvironmentSetup').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
 	});
 
-	$('.golangBuild').click(function(){
+	// and so on for rest of the links.
+	$('#golangBuild').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
 	 });
 
-	 $('.golangVariables').click(function(){
+	// and so on for rest of the links.
+	$('#golangVariables').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
 	 });
 
-	$('.golangPackage').click(function(){
+	// and so on for rest of the links.
+	$('#golangPackage').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
 	});
 
 });
-
-// Function for click links.
-
-// $(document).ready(function(){
-
-// $('.golangEnvironmentSetup').click(function(){
-// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
-// 	});
-
-// });
-
-
-// $(document).ready(function(){
-// $('.golangBuild').click(function(){
-// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
-//  });
-
-// });
-
-
-// $(document).ready(function(){
-// $('.golangVariables').click(function(){
-// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
-//  });
-// });
-
-
-// $(document).ready(function(){
-// $('.golangPackage').click(function(){
-// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
-// 	});
-// });
