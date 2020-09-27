87db919f2bfce04a653ca4c57d6df5956580dc36
amund <amund@akamai.com>
2020-09-27 20:54:53 +0530

    update

--- scripts/golangLinks.js
+++ scripts/golangLinks.js
@@ -2,40 +2,56 @@
 
 $(document).ready(function(){
 
-	$('golangIntroduction').click(function(){
+	$('.golangIntroduction').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html')
+	});
+
+	$('.golangEnvironmentSetup').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
+	});
+
+	$('.golangBuild').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
+	 });
+
+	 $('.golangVariables').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
+	 });
+
+	$('.golangPackage').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
 	});
 
 });
 
 // Function for click links.
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-$('golangEnvironmentSetup').click(function(){
-	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
-	});
+// $('.golangEnvironmentSetup').click(function(){
+// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
+// 	});
 
-});
-
-
-$(document).ready(function(){
-$('golangBuild').click(function(){
-	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
- });
-
-});
+// });
 
 
-$(document).ready(function(){
-$('golangVariables').click(function(){
-	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
- });
-});
+// $(document).ready(function(){
+// $('.golangBuild').click(function(){
+// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
+//  });
+
+// });
 
 
-$(document).ready(function(){
-$('golangPackage').click(function(){
-	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
-	});
-});
+// $(document).ready(function(){
+// $('.golangVariables').click(function(){
+// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
+//  });
+// });
+
+
+// $(document).ready(function(){
+// $('.golangPackage').click(function(){
+// 	$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
+// 	});
+// });
