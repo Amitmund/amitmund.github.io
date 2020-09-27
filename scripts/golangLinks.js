7632d195331cbefd6d56be0384707f1db2a26c2c
amund <amund@akamai.com>
2020-09-27 18:05:18 +0530

    update

---
+++ scripts/golangLinks.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('golangIntroduction').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html')
+	});
+
+	$('golangEnvironmentSetup').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html')
+	});
+
+	$('golangBuild').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html')
+	});
+	
+
+	$('golangVariables').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html')
+	});
+
+
+	$('golangPackage').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html')
+	});
+
+
+
+});
+
