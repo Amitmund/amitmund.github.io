e242f1385db098847ff15b225d89eec0ddb94b8a
amund <amund@akamai.com>
2020-09-28 12:37:59 +0530

    update

---
+++ scripts/readmeGodLinks.js
@@ -0,0 +1,16 @@
+// You can put individual links in the following main block.
+// Make sure to use '.' for class and '#' for id
+
+// changed the id name not to match as onclick function name. 
+// JavaScript Error: Uncaught TypeError: foo is not a function
+// https://stackoverflow.com/questions/36874237/javascript-error-uncaught-typeerror-foo-is-not-a-function
+
+$(document).ready(function(){
+
+	// For the links.
+	$('#readmeGod22Steps').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/readme/god/22Steps.html')
+	});
+
+
+});
