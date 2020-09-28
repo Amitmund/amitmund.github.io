72205ddc6cb727a21eefc23b4a13010c43d5a52c
amund <amund@akamai.com>
2020-09-28 21:32:55 +0530

    update

--- scripts/learningsLinuxLinks.js
+++ scripts/learningsLinuxLinks.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 	// For the links.
-	$('#learningsLinuxScreen').click(learningsLinuxScreen(){
+	$('#learningsLinuxScreen').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/linux/screen.html')
 	});
 
-	$('#learningsLinuxTmux').click(learningsLinuxTmux(){
+	$('#learningsLinuxTmux').click(function(){
 		$('.Main').load('https://amitmund.github.io/blog/learnings/linux/tmux.html')
 	});
 
