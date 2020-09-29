45d610388c520e14da54054a091a84685c879ae8
amund <amund@akamai.com>
2020-09-29 13:56:37 +0530

    update

---
+++ scripts/learningsTemplatesLinks.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	// For the links.
+	$('#learningsTextFormatting').click(function(){
+		$('.Main').load('https://amitmund.github.io/blog/learnings/templates/Formatting.html')
+	});
+
+});
