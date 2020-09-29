b2732898ea331873a56ccfad6b872eae710bffe4
amund <amund@akamai.com>
2020-09-29 19:37:55 +0530

    update

--- scripts/golangLinks.js
+++ scripts/golangLinks.js
@@ -5,31 +5,42 @@
 // JavaScript Error: Uncaught TypeError: foo is not a function
 // https://stackoverflow.com/questions/36874237/javascript-error-uncaught-typeerror-foo-is-not-a-function
 
+
+// $(document).ready(function(){
+
+// 	// for golangIntroduction class.
+// 	$('#golangIntroduction').click(function(){
+// 		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html');
+// 	});
+// });
+
+
+
 $(document).ready(function(){
 
 	// for golangIntroduction class.
 	$('#golangIntroduction').click(function(){
-		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html');
+		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html');
 	});
 
 	// and so on for rest of the links.
 	$('#golangEnvironmentSetup').click(function(){
-		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html');
+		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangEnvironmentSetup.html');
 	});
 
 	// and so on for rest of the links.
 	$('#golangBuild').click(function(){
-		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html');
+		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangBuild.html');
 	 });
 
 	// and so on for rest of the links.
 	$('#golangVariables').click(function(){
-		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html');
+		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangVariables.html');
 	 });
 
 	// and so on for rest of the links.
 	$('#golangPackage').click(function(){
-		$('.Main').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html');
+		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangPackage.html');
 	});
 
 });
