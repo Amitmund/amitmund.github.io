c5df18efb588ef829827b43f93c951b0f6ce4d20
amund <amund@akamai.com>
2020-10-03 00:00:04 +0530

    update.

---
+++ js/golang.js
@@ -0,0 +1,26 @@
+// You can put individual links in the following main block.
+// Make sure to use '.' for class and '#' for id
+
+// changed the id name not to match as onclick function name. 
+// JavaScript Error: Uncaught TypeError: foo is not a function
+// https://stackoverflow.com/questions/36874237/javascript-error-uncaught-typeerror-foo-is-not-a-function
+
+
+// $(document).ready(function(){
+
+// 	// for golangIntroduction class.
+// 	$('#golangIntroduction').click(function(){
+// 		$('#loadData').load('https://amitmund.github.io/blog/learnings/golang/golangIntroduction.html');
+// 	});
+// });
+
+
+
+$(document).ready(function(){
+
+	// for golangIntroduction class.
+	$('#golangIntroduction').click(function(){
+		$('#loadData').load('/pages/golangIntroduction.html');
+	});
+
+});
