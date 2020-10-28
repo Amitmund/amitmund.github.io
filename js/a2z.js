7e1c3a44bebc0065e7a1be02e2c8573e1bedaad6
amund <amund@akamai.com>
2020-10-28 16:37:29 +0530

    update.

---
+++ js/a2z.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+
+	$('#a2zA').click(function(){
+		$('#loadData').load('/pages/a2zA.html');
+	});
+
+	$('#a2zB').click(function(){
+		$('#loadData').load('/pages/a2zB.html');
+	});
+
+	$('#a2zC').click(function(){
+		$('#loadData').load('/pages/a2zC.html');
+	});
+
+
+	$('#a2zD').click(function(){
+		$('#loadData').load('/pages/a2zD.html');
+	});
+
+	$('#a2zE').click(function(){
+		$('#loadData').load('/pages/a2zE.html');
+	});
+
+
+	$('#a2zF').click(function(){
+		$('#loadData').load('/pages/a2zF.html');
+	});
+
+	$('#a2zG').click(function(){
+		$('#loadData').load('/pages/a2zG.html');
+	});
+
+
+	$('#a2zH').click(function(){
+		$('#loadData').load('/pages/a2zH.html');
+	});
+
+	$('#a2zI').click(function(){
+		$('#loadData').load('/pages/a2zI.html');
+	});
+
+	$('#a2zJ').click(function(){
+		$('#loadData').load('/pages/a2zJ.html');
+	});
+
+	$('#a2zK').click(function(){
+		$('#loadData').load('/pages/a2zK.html');
+	});
+
+	$('#a2zL').click(function(){
+		$('#loadData').load('/pages/a2zL.html');
+	});
+
+	$('#a2zM').click(function(){
+		$('#loadData').load('/pages/a2zM.html');
+	});
+
+	$('#a2zN').click(function(){
+		$('#loadData').load('/pages/a2zN.html');
+	});
+
+	$('#a2zO').click(function(){
+		$('#loadData').load('/pages/a2zO.html');
+	});
+
+	$('#a2zP').click(function(){
+		$('#loadData').load('/pages/a2zP.html');
+	});
+
+	$('#a2zQ').click(function(){
+		$('#loadData').load('/pages/a2zQ.html');
+	});
+
+	$('#a2zR').click(function(){
+		$('#loadData').load('/pages/a2zR.html');
+	});
+
+	$('#a2zS').click(function(){
+		$('#loadData').load('/pages/a2zS.html');
+	});
+
+	$('#a2zT').click(function(){
+		$('#loadData').load('/pages/a2zT.html');
+	});
+
+	$('#a2zU').click(function(){
+		$('#loadData').load('/pages/a2zU.html');
+	});
+
+	$('#a2zV').click(function(){
+		$('#loadData').load('/pages/a2zV.html');
+	});
+
+	$('#a2zW').click(function(){
+		$('#loadData').load('/pages/a2zW.html');
+	});
+
+	$('#a2zX').click(function(){
+		$('#loadData').load('/pages/a2zX.html');
+	});
+
+	$('#a2zY').click(function(){
+		$('#loadData').load('/pages/a2zY.html');
+	});
+
+	$('#a2zZ').click(function(){
+		$('#loadData').load('/pages/a2zZ.html');
+	});
+
+});
