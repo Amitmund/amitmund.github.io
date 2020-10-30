b71aeb9d05db101172e20a3df115d6c0e7385196
amund <amund@akamai.com>
2020-10-30 18:09:56 +0530

    update.

--- js/golang.js
+++ js/golang.js
@@ -15,6 +15,13 @@
 // });
 
 
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+
+
+
+
 
 $(document).ready(function(){
 
@@ -23,9 +30,13 @@ $(document).ready(function(){
 		$('#loadData').load('/pages/golangIntroduction.html');
 	});
 
-
+	
 
 });
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 
 
 // $(document).ready(function(){
@@ -64,3 +75,44 @@ $(document).ready(function(){
 	hljs.configure({useBR: true});
 	hljs.highlightBlock($('div').get(0));
 </script> */}
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+
+// quick jquery note //
+
+// Basic syntax is: $(selector).action()
+
+// A $ sign to define/access jQuery
+// A (selector) to "query (or find)" HTML elements
+// A jQuery action() to be performed on the element(s)
+
+
+// Examples:
+
+// $(this).hide() - hides the current element.
+
+// $("p").hide() - hides all <p> elements.
+
+// $(".test").hide() - hides all elements with class="test".
+
+// $("#test").hide() - hides the element with id="test".
+
+
+
+// $(document).ready(function(){
+
+//   // jQuery methods go here...
+
+// });
+
+
+// So: $(selector).action()
+
+
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
