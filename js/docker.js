704dc482a914e0e1f452241fa08b783df2eb9e65
amund <amund@akamai.com>
2020-11-24 19:55:15 +0530

    update.

---
+++ js/docker.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('#dockerQuickCommands').click(function(){
+		$('#loadData').load('/pages/dockerQuickCommands.html');
+	});
+
+	$('#dockerChapter1').click(function(){
+		$('#loadData').load('/pages/dockerChapter1.html');
+	});
+
+	
+
+});
